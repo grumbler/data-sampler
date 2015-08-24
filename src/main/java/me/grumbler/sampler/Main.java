8336ad90b40a0d888e0b02358054c48fe21bb04f
grumbler <serge.travin@gmail.com>
2015-08-24 17:14:32 +0300

    Initial project structure.

---
+++ src/main/java/me/grumbler/sampler/Main.java
@@ -0,0 +1,11 @@
+package me.grumbler.sampler;
+
+/**
+ * sampler
+ * me.grumbler.sampler
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
---
+++ src/test/java/me/grumbler/sampler/tests/MainTest.java
@@ -0,0 +1,17 @@
+package me.grumbler.sampler.tests;
+
+import me.grumbler.sampler.Main;
+import org.junit.Test;
+
+/**
+ * sampler
+ * me.grumbler.sampler.tests
+ */
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+        String[] args = new String[0];
+        Main.main(args);
+    }
+}
